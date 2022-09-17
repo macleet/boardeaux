1eb795e87552ef794b63f7e328fec7d441aab536
Mac Lee <seemin0506@gmail.com>
2022-09-16 19:23:04 -0700

    Update index.js
    Added relevant comments/header
    Included the sql.js module that was created
    Added a handler for GET requests for student table.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-/* Filename:     index.js
- * Author:       Mac Lee
- * Created:      9/13/2022
- * School:       UC Davis
- * Purpose:      Screening test
- * Description:  Main server file
+/* index.js is the main server file
+ **********************************
+ * In-place:
+ *      Student table GET request handler
+ *      User input student data POST request handler
+ * To-do:
+ *      Duplicate validation for student data when input and respond accordingly
+ *      Email authentication i.e. whether it is REAL UCD email (may need some sort of UCD API + key?)
+ *      Remove rows that are older than 6 years (At most 5? Arbitrary?)
  */
 
 // Load Express module
@@ -11,7 +14,7 @@ const express = require('express');
 // Body parsing middleware, i.e. req.body
 const bodyParser = require('body-parser');
 // Possibly require DB modules below when/if needed
-/* DB MODULE(S) */
+const db = require("./sql");
 // Express object
 const app = express();
 
@@ -36,13 +39,23 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-app.post('/studentData', (req, res) => {
+app.get('/getStudentTable', async (req, res) => {
+    const getAllCmd = 'SELECT * FROM StudentTable';
+    try {
+        let fullTable = await db.all(getAllCmd);
+        res.send(fullTable);
+    } catch(err) { console.log('Error: ', err); }
+});
+
+app.post('/studentData', async (req, res) => {
     let fname = req.body.firstname;
     let lname = req.body.lastname;
     let email = req.body.email;
     let level = req.body.level;
 
-    console.log("###",fname, lname, email, level);
+    const insertStudentCmd = 'INSERT INTO StudentTable (firstname, lastname, email, level) values (?, ?, ?, ?)';
+    await db.run(insertStudentCmd, [fname, lname, email, level]);
+    // console.log("###",fname, lname, email, level);
 
     res.send( { answer: '42' } );
 });
